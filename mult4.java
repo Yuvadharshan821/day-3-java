74953330dd6e5f7136fd45196550b50560cc828b
Yuvadharshan821 <yuvadharshan2005@gmail.com>
2024-12-04 13:45:19 +0530

    Add files via upload

---
+++ mult4.java
@@ -0,0 +1,14 @@
+public class mult4
+{
+    public static void main(String[] args){
+        int a=10;
+        if((a&3)==0)
+        {
+            System.out.println("multiple of 4");
+        }
+        else
+        {
+            System.out.println("not a multiple of 4");
+        }
+    }
+}
