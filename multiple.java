4dc6c0d222d909b5d66c68eee10f24dc2f723625
Yuvadharshan821 <yuvadharshan2005@gmail.com>
2024-12-04 12:30:08 +0530

    Add files via upload

---
+++ multiple.java
@@ -0,0 +1,14 @@
+public class multiple
+{
+    public static void main(String[] args){
+        int a=8;
+        if((a&(a-2))==0)
+        {
+            System.out.println("multiple of 4");
+        }
+        else
+        {
+            System.out.println("not a multiple of 4");
+        }
+    }
+}
