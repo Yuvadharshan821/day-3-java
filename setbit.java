20098b9b7434e926e4b12bc58ce60eab88bbac14
Yuvadharshan821 <yuvadharshan2005@gmail.com>
2024-12-04 11:06:52 +0530

    Add files via upload

---
+++ setbit.java
@@ -0,0 +1,15 @@
+public class setbit
+{
+    public static void main(String[] args){
+        int a=10;
+        int pos=2;
+        if((a&(1<<pos))!=0)
+        {
+            System.out.println("set bit");   
+        }
+        else
+        {
+            System.out.println("not a setbit");
+        }
+    }
+}
