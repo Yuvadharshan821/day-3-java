f231c8be9f826c6f7744f5653f273f32c374a8f2
Yuvadharshan821 <yuvadharshan2005@gmail.com>
2024-12-04 10:16:46 +0530

    Add files via upload

---
+++ rightshift.java
@@ -0,0 +1,9 @@
+public class rightshift
+{
+    public static void main(String[] args)
+    {
+        System.out.println(22>>2);
+        System.out.println(28>>3);
+        System.out.println(11>>1);
+    }
+}
