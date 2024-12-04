04e6f478f7598289f2543e0826f71389c335f6cc
Yuvadharshan821 <yuvadharshan2005@gmail.com>
2024-12-04 14:10:28 +0530

    Add files via upload

---
+++ smallest.java
@@ -0,0 +1,20 @@
+public class smallest
+{
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=34;
+        int c=2;
+        if(a<b&&a<c)
+        {
+            System.out.println("A is smaller");
+        }
+        else if(b<a&&b<c)
+        {
+            System.out.println("b is smaller");
+        }
+        else{
+            System.out.println("c is smaller");
+        }
+    }
+}
