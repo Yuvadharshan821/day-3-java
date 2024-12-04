ccfc0cde26d787af7ee45a7eb765903bf5500cda
Yuvadharshan821 <yuvadharshan2005@gmail.com>
2024-12-04 14:23:32 +0530

    Add files via upload

---
+++ gratest.java
@@ -0,0 +1,20 @@
+public class gratest
+{
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=34;
+        int c=2;
+        if(a>b&&a>c)
+        {
+            System.out.println("A is grater  ");
+        }
+        else if(b>a&&b>c)
+        {
+            System.out.println("b is grater");
+        }
+        else{
+            System.out.println("c is grater");
+        }
+    }
+}
